a2095771b2f19d9f5a896f62f7519af99920c65a
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-08-04 13:58:59 -0300

    Background now has a texture and isn't just a black rectangle

--- src/main/kotlin/me/steven/minestudio/gui/wigets/WorkspacePanel.kt
+++ src/main/kotlin/me/steven/minestudio/gui/wigets/WorkspacePanel.kt
@@ -2,11 +2,12 @@ package me.steven.minestudio.gui.wigets
 
 import io.github.cottonmc.cotton.gui.client.ScreenDrawing
 import io.github.cottonmc.cotton.gui.widget.WGridPanel
+import me.steven.minestudio.utils.identifier
 import net.minecraft.client.util.math.MatrixStack
 
 class WorkspacePanel : WGridPanel() {
     override fun paint(matrices: MatrixStack?, x: Int, y: Int, mouseX: Int, mouseY: Int) {
-        ScreenDrawing.coloredRect(x, y, width, height, 0xff000000.toInt())
+        ScreenDrawing.texturedRect(x, y, width, height, BACKGROUND_ID, -1)
 
         for (child in children) {
             child.paint(matrices, x + child.x, y + child.y, mouseX - child.x, mouseY - child.y)
@@ -14,4 +15,8 @@ class WorkspacePanel : WGridPanel() {
     }
 
     override fun canResize(): Boolean = false
+
+    companion object {
+        val BACKGROUND_ID = identifier("textures/gui/background.png")
+    }
 }

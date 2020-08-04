d5a2c6388a20c21cd96d3402012d8a80fe68b813
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-08-04 16:40:39 -0300

    Stretch a little the size of the workspace background

--- src/main/kotlin/me/steven/minestudio/gui/wigets/WorkspacePanel.kt
+++ src/main/kotlin/me/steven/minestudio/gui/wigets/WorkspacePanel.kt
@@ -7,7 +7,7 @@ import net.minecraft.client.util.math.MatrixStack
 
 class WorkspacePanel : WGridPanel() {
     override fun paint(matrices: MatrixStack?, x: Int, y: Int, mouseX: Int, mouseY: Int) {
-        ScreenDrawing.texturedRect(x, y, width, height, BACKGROUND_ID, -1)
+        ScreenDrawing.texturedRect(x - 8, y, width + 16, height, BACKGROUND_ID, -1)
 
         for (child in children) {
             child.paint(matrices, x + child.x, y + child.y, mouseX - child.x, mouseY - child.y)
